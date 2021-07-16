b4d39d1ee5ccf24a9b2f34c031eefa0f8a2a47c1
Deofino <guilhermedelfino25@gmail.com>
2021-07-15 21:32:46 -0300

    :sparkles: Adicionando Footer e Main (Formulario)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './app/components/Header/Header';
-
+import Main from './app/components/Main/Main';
+import Footer from './app/components/Footer/Footer';
 function App() {
   return (
     <>
       <Header />
-      
+      <Main />
+      <Footer />
     </>
   );
 }
