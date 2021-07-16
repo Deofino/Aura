2b3ef4fe18a64abaae78948d61edbadf8e0667de
Deofino <guilhermedelfino25@gmail.com>
2021-07-15 21:44:05 -0300

    :sparkles: Footer

---
+++ src/app/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Footer extends React.Component {
+    render() {
+        return (
+            <footer className="l-footer mt">
+                <div className="padding text-center">
+                    <span className="text-small">Todos os direitos Reservados por REACT &copy; 2021.</span>
+                </div>
+            </footer>
+        )
+    }
+}
