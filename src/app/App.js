8e2361868aeb147f9c6859cb9931a3edb6fd6402
Deofino <guilhermedelfino25@gmail.com>
2021-07-21 16:12:32 -0300

    :pencil2: Algumas alteracoes razas

---
+++ src/app/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './components/Header/Header';
+import Main from './components/Main/Main';
+import Footer from './components/Footer/Footer';
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
