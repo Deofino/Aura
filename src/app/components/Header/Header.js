4a1af1bff9490b43461f2dbc679373c81c87ffb0
Deofino <guilhermedelfino25@gmail.com>
2021-07-22 21:57:34 -0300

    :recycle: Alterando extensao arquivos

--- src/app/components/Header/Header.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Navbar from './Navbar';
-export default function Header(props) {
-    return (
-        <header className='l-headerMain relative shadow'>
-            <Navbar />
-        </header>
-    );
-}
